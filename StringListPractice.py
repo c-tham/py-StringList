f8ed88205252a004e8540b61cd05328380b26fbd
Chia T <tham_oh@hotmail.com>
2018-01-05 21:44:04 -0600

    add comments why .sort not working

--- StringListPractice.py
+++ StringListPractice.py
@@ -37,6 +37,8 @@ print x_new
 # New List
 # Start with a list like this one: x = [19,2,54,-2,7,12,98,32,10,-3,6]. Sort your list first. Then, split your list in half. Push the list created from the first half to position 0 of the list created from the second half. The output should be: [[-3, -2, 2, 6, 7], 10, 12, 19, 32, 54, 98]. Stick with it, this one is tough!
 
+### why .sort is not working? ###
+
 x = [19,2,54,-2,7,12,98,32,10,-3,6]
 print x
 x_new = sorted(x)
